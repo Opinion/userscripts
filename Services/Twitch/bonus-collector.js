ff5cdea71bd532be51e025ccc19046afe87fbc5d
Aleksander <58629082+Opinion@users.noreply.github.com>
2024-04-21 19:43:21 +0200

    Add v1.0 for Twitch/bonus-collector.js

--- Services/Twitch/bonus-collector.js
+++ Services/Twitch/bonus-collector.js
@@ -1,1 +1,28 @@
+// ==UserScript==
+// @name             [bonus-collector] Twitch
+// @description      Automatically collect bonus in live channels.
+// @match            *://*.twitch.tv/*
+// @version          1.0
+// @updateURL        https://github.com/Opinion/userscripts/raw/main/Twitch/bonus-collector.js
+// @downloadURL      https://github.com/Opinion/userscripts/raw/main/Twitch/bonus-collector.js
+// ==/UserScript==
 
+(async () => {
+  function log(...args) {
+    console.log('[bonus-collector]', ...args);
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  log('Started.');
+  log('You can find more of my userscripts here; https://github.com/Opinion/userscripts');
+  while (true) {
+    const button = document.querySelector('button[aria-label="Claim Bonus"]');
+    if (button) {
+      button.click();
+    }
+    await sleep(10000);
+  }
+})();
